01093c2121666156d5aa025a9377556d26e32ce2
Kris Lyon <kris.lyon@gmail.com>
2021-07-30 13:30:27 +0000

    updated.w :q

--- htb-recon.sh
+++ htb-recon.sh
@@ -2,51 +2,55 @@
 
 target=$1
 name=$2
-box=$3
+targetOs=$3
+vpnConfig=$4
 
-if (($#==2));
+if (($#==4));
 then
 
-  ## Create directory
+  ## Create & configure directory
   mkdir -p ~/HackSpace/$name
   mkdir -p ~/HackSpace/$name/recon
   mkdir -p ~/HackSpace/$name/loot
   mkdir -p ~/HackSpace/$name/exploits
+  cp $vpnConfig ~/HackSpace/$name/htb-vpn.ovpn
+
+  ## Create notes
+  echo "# Name: $name" >> ~/HackSpace/$name/notes.md
+  echo "# IP: $target" >> ~/HackSpace/$name/notes.md
+  echo "# TargetOS: $targetOs" >> ~/HackSpace/$name/notes.md
+  echo "# Level:" >> ~/HackSpace/$name/notes.md
+  echo "#" >> ~/HackSpace/$name/notes.md
 
   ## Create session
   cd ~/HackSpace/$name
   export target
-  export box
+  export targetOs
   export name
 
   ## Create Tmux Session
-  tmux new -s htb_recon -d
-  tmux rename-window -t htb_recon VPN
+  tmux new -s $name -d
+  tmux rename-window -t $name VPN
   tmux split-window -v -t VPN
-  tmux split-window -h -t htb_recon:VPN.0
-  tmux new-window -d -t htb_recon -n NOTES
-  tmux new-window -d -t htb_recon -n DASH
-  tmux split-window -h -t htb_recon:DASH.0
-  tmux split-window -v -t htb_recon:DASH.1
-  tmux split-window -v -t htb_recon:DASH.0
+  tmux split-window -h -t $name:VPN.0
+  tmux new-window -d -t $name -n NOTES
+  tmux new-window -d -t $name -n RECON
+  tmux split-window -h -t $name:RECON.0
+  tmux split-window -v -t $name:RECON.1
+  tmux split-window -v -t $name:RECON.0
 
   ## Running Commands
-  tmux send-keys -t htb_recon:VPN.0 'sudo openvpn' 
+  tmux send-keys -t $name:VPN.0 'sudo openvpn htb-vpn.ovpn' 
 
-  tmux send-keys -t htb_recon:DASH.0 'nmap -sC -sV -oN recon/initial_nmap.txt -v -Pn $target' 
-  tmux send-keys -t htb_recon:DASH.1 'sudo masscan -p1-65535 -e tun0 -oL recon/allports.txt --rate=1000 -vv -Pn $target'
-  tmux send-keys -t htb_recon:DASH.2 'gobuster dir -u http://$target -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o recon/dirscan.txt'
-  tmux send-keys -t htb_recon:DASH.2 '/opt/ffuf -w /usr/share/wordlists/dirb/big.txt -u http://$target/FUZZ | tee recon/ffuf.txt' 
+  tmux send-keys -t $name:RECON.0 'nmap -sC -sV -oN recon/initial_nmap.txt -v -Pn $target' 
+  tmux send-keys -t $name:RECON.1 'sudo masscan -p1-65535 -e tun0 -oL recon/allports.txt --rate=1000 -vv -Pn $target'
+  tmux send-keys -t $name:RECON.2 'gobuster dir -u http://$target -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o recon/dirscan.txt'
+  tmux send-keys -t $name:RECON.2 '/opt/ffuf -w /usr/share/wordlists/dirb/big.txt -u http://$target/FUZZ | tee recon/ffuf.txt' 
 
-  tmux send-keys -t htb_recon:NOTES 'echo "# Info" >> Notes.md' 
-  tmux send-keys -t htb_recon:NOTES 'echo "* Name: $name" >> Notes.md' 
-  tmux send-keys -t htb_recon:NOTES 'echo "* IP: $target" >> Notes.md' 
-  tmux send-keys -t htb_recon:NOTES 'echo "* Box: $box" >> Notes.md' 
-  tmux send-keys -t htb_recon:NOTES 'echo "* Level: " >> Notes.md' 
-  tmux send-keys -t htb_recon:NOTES 'vi Notes.md'
+  tmux send-keys -t $name:NOTES 'vi notes.md'
 
 else
-  echo "Usage: ./htb-recon.sh <IP> <Name_of_Machine> <OS> "
-  echo "Example: ./workspace.sh 10.10.10.180 ServMon Windows"
+  echo "Usage: ./htb-recon.sh <IP> <Name_of_Machine> <OS> <Path to VPN Config>"
+  echo "Example: ./workspace.sh 10.10.10.180 ServMon Windows ../starting_point.ovpn"
 
 fi
